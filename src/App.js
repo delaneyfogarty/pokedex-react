c5e593e73b36dc8d2a637140c0066b50f368242e
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-16 14:38:18 -0700

    set up app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getPoke } from './services/fetch-utils';
 
 function App() {
+  const [poke, setPoke] = useState([]);
+  const [query, setQuery] = useState('chu');
+
+  async function load() {
+    const data = await getPoke(query);
+
+    setPoke(data.data.results);
+  }
+
+  useEffect(() => {
+    load();
+  }, []); //eslint-disable-line
+
+  async function handleSearch(e) {
+    e.preventDefault();
+    load();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form onSubmit={handleSearch}>
+        <input onChange={(e) => setQuery(e.target.value)} />
+        <button>Search</button>
+      </form>
+      <header className="app-header">
+        {poke.map((x, i) => (
+          <div key={x.pokemon + i}>
+            <h2>{x.pokemon}</h2>
+            <p>{x.attack}</p>
+            <p>{x.defense}</p>
+            <img src={x.url_image} />
+          </div>
+        ))}
       </header>
     </div>
   );
